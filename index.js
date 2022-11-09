b00d679adc33dc7cca039ce08f4e9fac23c790e9
sabikonnahar.sr <sabikonnahar.sr@gmail.com>
2022-11-09 12:34:19 +0600

    added review

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ async function run(){
 
 
           
-            app.get(`/reviewServices/:id`, async (req, res) => {
+            app.get(`/addServices/:id`, async (req, res) => {
                 const id = req.params.id;
                 const query = { _id: ObjectId(id) };
                 const resultById = await serviceCollection.findOne(query);
