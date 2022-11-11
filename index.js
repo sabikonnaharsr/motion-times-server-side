32277cc79a052b6e26440b3ad89a9bd7b6f65060
sabikonnahar.sr <sabikonnahar.sr@gmail.com>
2022-11-11 23:24:46 +0600

    added deltete

--- index.js
+++ index.js
@@ -10,9 +10,6 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
-// user: sequelExtractDb
-// pass: 1j0PJa4mtnF16osm
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.svbs0rh.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
 const client = new MongoClient(uri, {
@@ -38,7 +35,7 @@ async function run() {
       res.send(services);
     });
 
-    //   allServices get from express js
+    // allServices get from express js
     app.get("/allServices", async (req, res) => {
       const query = {};
       const cursor = serviceCollection.find(query);
@@ -46,7 +43,7 @@ async function run() {
       res.send(services);
     });
 
-    /// sggll
+    // allService collection
     app.post("/allServices", async (req, res) => {
       const services = req.body;
       console.log(services);
@@ -55,43 +52,54 @@ async function run() {
     });
 
     //asdd servi
-    -(
-      // app.get(`/addServices/:id`, async (req, res) => {
-      //     const id = req.params.id;
-      //     const query = { _id: ObjectId(id) };
-      //     const resultById = await serviceCollection.findOne(query);
-      //     res.send(resultById);
 
-      // });
+    // app.get(`/addServices/:id`, async (req, res) => {
+    //     const id = req.params.id;
+    //     const query = { _id: ObjectId(id) };
+    //     const resultById = await serviceCollection.findOne(query);
+    //     res.send(resultById);
 
-      app.get(`/services/:id`, async (req, res) => {
-        const id = req.params.id;
-        const query = { _id: ObjectId(id) };
-        const resultById = await serviceCollection.findOne(query);
-        res.send(resultById);
-      })
-    );
+    // });
 
-    //orders get all orders from client
+    //details page by id
+    app.get(`/services/:id`, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const resultById = await serviceCollection.findOne(query);
+      res.send(resultById);
+    });
+
+    //choose get all packages from client
     app.post("/reviewByMail", async (req, res) => {
       const user = req.body;
       const result = await weddingCollection.insertOne(user);
       res.send(result);
     });
 
+
+
+
     // patch update
-    app.patch(`/updateReview/:id`, async (req, res) => {
+    app.put("/review/:id", async (req, res) => {
       const id = req.params.id;
-      const status = req.body.status;
-      const query = { _id: ObjectId(id) };
+      const filter = { _id: ObjectId(id) };
+      const updatedUser = req.body;
+      const option = { upsert: true };
       const updateDoc = {
         $set: {
-          status: status,
+          message: updatedUser.message,
         },
       };
-      const result = await orderCollection.updateOne(query, updateDoc);
+      const result = await weddingCollection.updateOne(
+        filter,
+        updateDoc,
+        option
+      );
       res.send(result);
     });
+
+
+
 
     // // delete
     // app.delete("/deleteReview/:id", async (req, res) => {
@@ -102,7 +110,7 @@ async function run() {
     //     console.log(result)
     // });
 
-    //review client sabikonnahar
+    // single person review collection by email
     app.get("/reviewByMail", async (req, res) => {
       let query = {};
       if (req.query.email) {
@@ -116,14 +124,46 @@ async function run() {
       res.send(review);
     });
 
-    //all client reviews sabikonnahar
-    app.get("/allReview", async (req, res) => {
-      id = req.query.serviceId;
-      let query = { serviceId: id };
+    //for clients all review collection
+    app.get(`/reviewId`, async (req, res) => {
+      let query = {};
+      if (req.query.serviceId) {
+        query = {
+          serviceId: req.query.serviceId,
+        };
+      }
+      const searchService = weddingCollection.find(query);
+      const result = await searchService.toArray();
+
+      res.send(result);
+    });
+
+    app.get("/review/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
       const cursor = weddingCollection.find(query);
       const review = await cursor.toArray();
       res.send(review);
     });
+
+    // remove review
+
+app.delete('/review/:id', async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: ObjectId(id) };
+  const result = await weddingCollection.deleteOne(query);
+  res.send(result);
+})
+
+
+    //edit single review for a person//can see all
+    // app.get("/allReview/:allReviewId", async (req, res) => {
+    //   const id = req.params.allReviewId;
+    //   let query = { serviceId: id };
+    //   const cursor = weddingCollection.find(query);
+    //   const review = await cursor.toArray();
+    //   res.send(review);
+    // });
   } finally {
   }
 }
@@ -137,11 +177,3 @@ app.listen(port, () => {
   console.log(`Sequel Extract is running${port}`);
 });
 
-//remove review
-
-// app.delete('/review/:id', async (req, res) => {
-//     const id = req.params.id;
-//     const query = { _id: ObjectId(id) };
-//     const result = await reviewCollection.deleteOne(query);
-//     res.send(result);
-// })
